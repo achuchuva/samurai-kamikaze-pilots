db831df1d408298600cc81b10439521ba16cbeab
achuchuva <67221090+achuchuva@users.noreply.github.com>
2023-05-02 16:24:51 +1000

    Create settings.php

---
+++ settings.php
@@ -0,0 +1,8 @@
+<?php
+  # Settings php for database
+  # Currently defauly values - remove this comment when modified
+  $host = "feenix.mariadb.swin.edu.au";
+  $user = "s1234567";
+  $pwd = "password";
+  $sql_db = " s1234567_db";
+?>
