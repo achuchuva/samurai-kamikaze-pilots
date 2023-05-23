868e377b85aa157b95ed58a698223ca8fe658048
Shandor101 <127173379+Shandor101@users.noreply.github.com>
2023-05-23 15:13:53 +1000

    PHP meta

--- about.php
+++ about.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="keywords" content="HTML, CSS">
+    <meta name="keywords" content="HTML, CSS, PHP">
     <meta name="author" content="Anton Chuchuva">
     <meta name="description" content="About the SKP Secret Key Productions team">
     <link rel="stylesheet" href="styles/style.css">
