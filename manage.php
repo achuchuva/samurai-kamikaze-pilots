1f2e16c91b01e745d715c00bd54fb5a4cbf8f179
WilliamReedie <104300597@student.swin.edu.au>
2023-05-23 15:03:00 +1000

    manage link
    adds link to manage page in navbar
    adds head to manage page
    adds manage.css file

--- manage.php
+++ manage.php
@@ -1,3 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=0.5">
+    <meta name="keywords" content="HTML, CSS"> <!-- Should we add PHP to all of these? -->
+    <meta name="author" content="Amile Kercher">
+    <meta name="description" content="SKP manage page">
+    <link rel="stylesheet" href="styles/manage.css">
+    <title>SKP - Log In</title>
+</head>
+<body>
 <?php
 
 require_once("settings.php");
@@ -19,3 +33,7 @@ if (!$conn) {
 
 
 ?>
+
+</body>
+
+</html>
