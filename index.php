ae65b55348234269c4b32fb1f2d132cb7381ae7d
achuchuva <67221090+achuchuva@users.noreply.github.com>
2023-06-01 21:29:44 +1000

    Done

--- index.php
+++ index.php
@@ -50,7 +50,8 @@
                     Thank you for considering us as your potential employer and we look forward to reviewing
                     your application.
                 </p>
-                <a href="https://www.youtube.com/watch?v=6bJt4Fd_mwk">Link to group video</a>
+                <a href="https://www.youtube.com/watch?v=6bJt4Fd_mwk">Link to group video assignment 1</a>
+                <a href="https://www.youtube.com/watch?v=39-a6K3Ch7k">Link to group video assignment 2</a>
                 <br>
                 <br>
                 <a class="button" href="#footer">
